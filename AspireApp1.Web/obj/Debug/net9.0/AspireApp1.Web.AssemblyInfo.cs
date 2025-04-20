c74d47b2d6b1c9f166039a5aea76832d83b999a4
claudehang <claudehang@qq.com>
2025-04-20 17:43:07 +0800

    add seat map

--- AspireApp1.Web/obj/Debug/net9.0/AspireApp1.Web.AssemblyInfo.cs
+++ AspireApp1.Web/obj/Debug/net9.0/AspireApp1.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspireApp1.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6cf11cbc7b880e158beb06339f974c9bfa8a35d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5abb4f61b7b3f7e947b028e653a183c09b39849")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspireApp1.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspireApp1.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
